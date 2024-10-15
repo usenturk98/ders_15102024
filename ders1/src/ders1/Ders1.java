aaceb125d3ffd32d472a2668908daf3a62f117d1
aidata <aidata@10.121.3.83>
2024-10-15 09:17:22 +0300

    first

---
+++ ders1/src/ders1/Ders1.java
@@ -0,0 +1,11 @@
+package ders1;
+
+public class Ders1 {
+
+	public static void main(String[] args) {// main fonksiyonu
+		System.out.print("Merhaba JAVA");
+		System.out.println();
+
+	}
+
+}
---
+++ ders1/src/ders1/Ders1_Scan.java
@@ -0,0 +1,10 @@
+package ders1;
+
+public class Ders1_Scan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
